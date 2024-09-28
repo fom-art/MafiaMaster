74bee0305012584a57ecfe8321b880c25ff0354f
anfo-code <artem29040506000@gmail.com>
2024-09-28 16:19:05 +0200

    :lipstick: Finish setup screen creation

---
+++ core/ui/src/main/kotlin/com/fomart/mafiamaster/core/ui/components/RoleItemComponent.kt
@@ -0,0 +1,193 @@
+package com.fomart.mafiamaster.core.ui.components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.fomart.mafiamaster.core.model.setup.RoleItem
+import com.fomart.mafiamaster.core.resources.R
+import com.fomart.mafiamaster.core.ui.theme.MafiaMasterTheme
+
+@Composable
+fun RoleItemComponent(
+    modifier: Modifier = Modifier,
+    roleItem: RoleItem
+) {
+    val animationDuration = 350
+    val animationSpec = tween<Color>(
+        durationMillis = animationDuration,
+        easing = FastOutLinearInEasing
+    )
+
+    // Get animated colors based on whether the role item is selected
+    val animatedContainerColor = getAnimatedColor(
+        isSelected = roleItem.isSelected,
+        selectedColor = MaterialTheme.colorScheme.primary.copy(alpha = 1f),
+        unselectedColor = MaterialTheme.colorScheme.surfaceContainer,
+        animationSpec = animationSpec
+    )
+
+    val animatedContentColor = getAnimatedColor(
+        isSelected = roleItem.isSelected,
+        selectedColor = MaterialTheme.colorScheme.onPrimary,
+        unselectedColor = MaterialTheme.colorScheme.onSurface,
+        animationSpec = animationSpec
+    )
+
+    val animatedIconColor = getAnimatedColor(
+        isSelected = roleItem.isSelected,
+        selectedColor = MaterialTheme.colorScheme.primary.copy(alpha = 1f),
+        unselectedColor = MaterialTheme.colorScheme.onSurface,
+        animationSpec = animationSpec
+    )
+
+    // Define the card colors
+    val cardColors = getCardColors(
+        containerColor = animatedContainerColor,
+        contentColor = animatedContentColor
+    )
+
+    // Render the Card component
+    RoleCard(
+        modifier = modifier,
+        roleItem = roleItem,
+        cardColors = cardColors,
+        animatedIconColor = animatedIconColor
+    )
+}
+
+// Function to animate color change
+@Composable
+private fun getAnimatedColor(
+    isSelected: Boolean,
+    selectedColor: Color,
+    unselectedColor: Color,
+    animationSpec: AnimationSpec<Color>
+): Color {
+    val targetColor = if (isSelected) selectedColor else unselectedColor
+    val animatedColor by animateColorAsState(
+        targetValue = targetColor,
+        animationSpec = animationSpec,
+        label = ""
+    )
+    return animatedColor
+}
+
+// Function to define card colors
+@Composable
+private fun getCardColors(containerColor: Color, contentColor: Color): CardColors {
+    return CardDefaults.cardColors().copy(
+        containerColor = containerColor,
+        contentColor = contentColor
+    )
+}
+
+// Function to render the card UI
+@Composable
+private fun RoleCard(
+    modifier: Modifier,
+    roleItem: RoleItem,
+    cardColors: CardColors,
+    animatedIconColor: Color
+) {
+    Card(
+        modifier = modifier
+            .padding(vertical = dimensionResource(id = R.dimen.padding_small))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { roleItem.onClick?.invoke() },
+        colors = cardColors
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium),
+                    vertical = dimensionResource(id = R.dimen.padding_medium)
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Display the circle icon
+            CircleIcon(
+                painter = painterResource(id = roleItem.icon),
+                iconColor = animatedIconColor
+            )
+
+            // Display the role title
+            RoleTitle(
+                title = stringResource(id = roleItem.title)
+            )
+
+            // Optionally display the role count
+            roleItem.count?.let {
+                RoleCountText(count = it)
+            }
+        }
+    }
+}
+
+// Function to display the role title
+@Composable
+private fun RowScope.RoleTitle(title: String) {
+    Text(
+        modifier = Modifier
+            .padding(horizontal = dimensionResource(R.dimen.padding_medium))
+            .weight(1f),
+        text = title,
+        style = MaterialTheme.typography.titleLarge,
+        fontWeight = FontWeight.Medium
+    )
+}
+
+// Function to display the role count
+@Composable
+private fun RoleCountText(count: Int) {
+    Text(
+        modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+        text = "$count",
+        style = MaterialTheme.typography.titleLarge,
+        fontWeight = FontWeight.Medium
+    )
+}
+
+@Preview
+@Composable
+fun RoleItemComponentPreview() {
+    var hasMistress by remember { mutableStateOf(false) }
+
+    MafiaMasterTheme {
+        RoleItemComponent(
+            roleItem = RoleItem(
+                title = R.string.role_mistress,
+                icon = R.drawable.ic_mistress,
+                isSelected = hasMistress,
+                onClick = { hasMistress = !hasMistress },
+            )
+        )
+    }
+}
--- core/ui/src/main/kotlin/com/fomart/mafiamaster/core/ui/screens/game/setup/SetupScreen.kt
+++ core/ui/src/main/kotlin/com/fomart/mafiamaster/core/ui/screens/game/setup/SetupScreen.kt
@@ -1,79 +1,30 @@
 package com.fomart.mafiamaster.core.ui.screens.game.setup
 
-import android.content.res.Configuration
-import android.util.Log
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.PagerDefaults
-import androidx.compose.foundation.pager.PagerSnapDistance
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
 import com.fomart.mafiamaster.core.model.setup.RoleItem
 import com.fomart.mafiamaster.core.model.setup.RoleDistributionModel
 import com.fomart.mafiamaster.core.ui.theme.MafiaMasterTheme
 import com.fomart.mafiamaster.core.resources.R
-import com.fomart.mafiamaster.core.ui.components.CircleIcon
 import com.fomart.mafiamaster.core.ui.components.CustomFilledButton
 import com.fomart.mafiamaster.core.ui.components.DefaultTopBar
-import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
-import kotlin.math.ceil
+import com.fomart.mafiamaster.core.ui.components.HorizontalPlayerCountPicker
+import com.fomart.mafiamaster.core.ui.components.RoleItemComponent
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultScaffoldScreen(
     modifier: Modifier = Modifier,
@@ -129,7 +80,7 @@ fun SetupBody(
                 RoleItemComponent(roleItem = roleItem)
             }
         }
-        PlayersCountSetterLayout(
+        HorizontalPlayerCountPicker(
             playerCounts = playerCounts,
             setPlayersTo = setPlayersTo
         )
@@ -137,148 +88,6 @@ fun SetupBody(
             onClick = startGame,
             text = stringResource(id = R.string.game_start),
         )
-    }
-}
-
-@Composable
-fun RoleItemComponent(
-    modifier: Modifier = Modifier,
-    roleItem: RoleItem
-) {
-    val animationDuration = 350
-
-    val containerColor =
-        if (roleItem.isSelected) MaterialTheme.colorScheme.primary.copy(alpha = 1f) else MaterialTheme.colorScheme.surfaceContainer
-    val contentColor =
-        if (roleItem.isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurface
-    val iconColor =
-        if (roleItem.isSelected) MaterialTheme.colorScheme.primary.copy(alpha = 1f) else MaterialTheme.colorScheme.onSurface
-
-    val animationSpec = tween<Color>(
-        durationMillis = animationDuration,
-        easing = FastOutLinearInEasing
-    )
-
-    val animatedContainerColor by animateColorAsState(
-        targetValue = containerColor,
-        animationSpec = animationSpec,
-        label = ""
-    )
-
-    val animatedContentColor by animateColorAsState(
-        targetValue = contentColor,
-        animationSpec = animationSpec,
-        label = ""
-    )
-
-    val animatedIconColor by animateColorAsState(
-        targetValue = iconColor,
-        animationSpec = animationSpec,
-        label = ""
-    )
-
-    val cardColors = CardDefaults.cardColors().copy(
-        containerColor = animatedContainerColor, contentColor = animatedContentColor
-    )
-
-    Card(
-        modifier = modifier
-            .padding(
-                vertical = dimensionResource(id = R.dimen.padding_small)
-            )
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null
-            ) { roleItem.onClick?.let { it() } },
-        colors = cardColors
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = dimensionResource(id = R.dimen.padding_medium),
-                    vertical = dimensionResource(id = R.dimen.padding_medium)
-                ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CircleIcon(
-                painter = painterResource(id = roleItem.icon),
-                iconColor = animatedIconColor
-            )
-            Text(
-                modifier = Modifier
-                    .padding(horizontal = dimensionResource(R.dimen.padding_medium))
-                    .weight(1f),
-                text = stringResource(id = roleItem.title),
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Medium
-            )
-            roleItem.count?.let {
-                Text(
-                    modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.padding_medium)),
-                    text = "${roleItem.count}",
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.Medium
-                )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun PlayersCountSetterLayout(
-    modifier: Modifier = Modifier,
-    playerCounts: List<Int>,
-    setPlayersTo: (Int) -> Unit
-) {
-    BoxWithConstraints(
-        modifier = Modifier
-            .fillMaxWidth(0.66f)
-            .padding(vertical = dimensionResource(id = R.dimen.padding_small))
-    ) {
-        val contentPadding = (maxWidth - 50.dp) / 2
-        val offSet = maxWidth / 3
-        val itemSpacing = offSet - 50.dp
-
-        // Set initial page based on the totalPlayers (find the index in playerCounts)
-        val pagerState = rememberPagerState(pageCount = { playerCounts.size })
-
-        LaunchedEffect(pagerState.currentPage) {
-            setPlayersTo(playerCounts[pagerState.currentPage])
-        }
-
-        HorizontalPager(
-            modifier = modifier.padding(vertical = dimensionResource(id = R.dimen.padding_medium)),
-            state = pagerState,
-            flingBehavior = PagerDefaults.flingBehavior(
-                state = pagerState,
-                pagerSnapDistance = PagerSnapDistance.atMost(0)
-            ),
-            contentPadding = PaddingValues(horizontal = contentPadding),
-            pageSpacing = itemSpacing,
-        ) { page ->
-            Box(
-                modifier = Modifier
-                    .graphicsLayer {
-                        val pageOffset = ((pagerState.currentPage - page) + pagerState
-                            .currentPageOffsetFraction).absoluteValue
-                        // Set the item alpha based on the distance from the center
-                        val percentFromCenter = 1.0f - (pageOffset / (5f / 2f))
-                        val opacity = 0.25f + (percentFromCenter * 0.75f).coerceIn(0f, 1f)
-
-                        alpha = opacity
-                        clip = true
-                    }
-            ) {
-                Text(
-                    text = "${playerCounts[page]}",
-                    modifier = Modifier
-                        .wrapContentHeight(),
-                    style = MaterialTheme.typography.displaySmall,
-                )
-            }
-        }
     }
 }
 
